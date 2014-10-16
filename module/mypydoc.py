4ad3b7b48ea626345ff3fdf819cf54f50fb6ed6a
Abhilash Km <aaabhilash97@gmail.com>
2014-10-16 13:31:06 +0530

    3.12

--- module/mypydoc.py
+++ module/mypydoc.py
@@ -1,7 +1,9 @@
 import sys
+from inspect import isfunction
 def mypydoc(x):
 	p=__import__(x)
 	print 'DESCRIPTION:\n----------\n',p.__doc__,'\nFUNCTIONS\n---------\n'
-	for y in dir(x):
-		print y
+	for y in dir(p):
+#		if isfunction(p.y):
+			print y
 mypydoc(sys.argv[1])
